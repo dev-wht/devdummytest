4eee4830405b3e3bc030dcf566a76e263a6184a7
dev-wht <dev-wht@users.noreply.github.com>
2016-06-05 11:54:43 +0530

    added 2 prints

--- hellopython.py
+++ hellopython.py
@@ -7,3 +7,7 @@ quotes = {
 }
 stooge = "Larry"
 print(stooge, "says:", quotes[stooge])
+
+language = 7
+print("Language %s: I am Python. What's for supper?")
+print(language, "I am Python. What's for supper?")
